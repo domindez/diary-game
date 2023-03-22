529fa0c8804829c36f0c65e1993bded81f739815
domindez <109239601+domindez@users.noreply.github.com>
2023-03-22 18:46:22 +0100

    analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './Sass/index.scss'
 import App from './App'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-KMDVRPL'
+}
+TagManager.initialize(tagManagerArgs)
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
