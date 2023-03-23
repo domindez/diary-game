2ad9ea3961c3c2fd472ac41be79f9ffb13b981c2
Daniel Dominguez <danidomin19@gmail.com>
2023-03-23 22:11:45 +0100

    userstorage

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 const isDev = process.env.NODE_ENV === 'development'
 
 export const API_BASE_URL = isDev ? 'http://localhost:4000' : 'https://backend-diary-game-production.up.railway.app'
-export const userStorage = 'diary-tfy-user'
+export const userStorage = 'diary-tfy-userv2'
 export const gameStorage = 'diary-tfy-game'
